be06cda710da54fd1a6b96624439a5f19ff865ae
laryfrancine <66083577+laryfrancine@users.noreply.github.com>
2020-05-28 15:27:00 -0400

    Add files via upload

---
+++ Coursera Applied Data Science Capstone Notebook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#This notebook will be mainly used for the capstone project
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!") 
+
+
+# In[ ]:
+
+
+
+
